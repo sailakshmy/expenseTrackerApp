9c92415eab13a367231d1d1170f601616b8a354c
sailakshmy <sailakshmy94@gmail.com>
2025-04-03 22:46:52 +0400

    Created a custom input component

---
+++ expense-tracker/components/ManageExpense/Input.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, TextInput, View } from "react-native";
+
+const Input = ({ label, textInputConfig }) => {
+  return (
+    <View>
+      <Text>{label}</Text>
+      <TextInput {...textInputConfig} />
+    </View>
+  );
+};
+
+export default Input;
